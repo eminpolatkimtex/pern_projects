c7f583b2c9abd6985c697bb7b729374fdcc3f7af
eminpolatkimtex <emin.polat@kimtex.com>
2025-09-20 11:37:26 +0300

    update

--- pern-stack/back-end/server.js
+++ pern-stack/back-end/server.js
@@ -16,6 +16,35 @@ app.use(cors()); // middleware
 app.use(helmet()); // middleware fot safety
 app.use(morgan("dev")); // logger 
 
+app.use(async (req,res,next) => {
+    try {
+        const desicion = await aj.protect(req, {
+            requested:1, // 
+        });
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                res.status(429).json({error:"Too many requests"});
+            } else if (decision.reason.isBot()) {
+                res.status(403).json({error:"Bot access denied"});
+            } else {
+                res.status(403).json({error:"Forbidden"});
+            }
+            return
+        }
+
+        if (decision.results.some((result) => result.reason.isBot() && result.isSpoofed())) {
+            res.status(403).json({error:"Spoofed bot detected"});
+            return;
+        }
+
+        next();
+    } catch (error) {
+        console.log("Arcjet error", error);
+        next(error);
+    }
+});
+
 app.use('/api/products', productRoutes);
 
 async function initDB() {
